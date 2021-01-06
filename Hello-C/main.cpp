863c7ab56a30f97242ee3f9a7be78783f53f6aee
wds <modi.wu@tuya.com>
2021-01-06 11:25:20 +0800

    Initial Commit

---
+++ Hello-C/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Hello-C
+//
+//  Created by wds on 2021/1/6.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
